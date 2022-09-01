1c6959262fd3a0ac1831ea93d8d86bc322d0586a
paul geana <87538456+1234pau@users.noreply.github.com>
2022-09-01 19:39:49 +0300

    search resp

--- script.js
+++ script.js
@@ -183,4 +183,3 @@ function showVideos(videos) {
     })
 
 }
-//<iframe width="560" height="315" src="https://www.youtube.com/embed/tgB1wUcmbbw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
